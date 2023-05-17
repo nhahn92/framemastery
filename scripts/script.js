77b6b4fac07e5f7a6bde82c63561ca50bd391373
Nicole Hahn <nicolehahn92@gmail.com>
2023-05-17 00:38:33 -0400

    Started utility function to populate HTML

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,38 @@
 // Retrieves data from various API-fetching modules
 import { getWarframesData } from "./api-retrieval/get-warframes.js";
+import { getModsData } from "./api-retrieval/get-mods.js";
+import { getWeaponsData } from "./api-retrieval/get-weapons.js";
 
-// getWarframesData();
+// Empty arrays to be populated with specific values
+let warframesNames = [];
+let warframesPrimeNames = [];
+let archwingsNames = [];
+let necramechsNames = [];
+
+// Note to self: May not need empty arrays at all and can use the "getValues" function to populate HTML
+// Utility function to populate empty arrays from retrieved API data through passed parameters
+// "Type" should be either "regular," "prime," "archwing," or "necramech"
+// "Array" should choose one of the empty arrays above
+// "ObjectProperty" should correspond with a property like "name" or "type" from the API json
+// "Category" should be something like "warframes," "weapons," "mods," et cetera
+async function getValues(type, array, objectProperty, category) {
+    let data;
+    if (category === "warframes") {
+        data = await getWarframesData(type);
+    }
+    data.forEach(element => {
+        array.push(element[objectProperty]);
+    })
+}
+
+getValues("regular", warframesNames, "name", "warframes");
+getValues("prime", warframesPrimeNames, "name", "warframes");
+getValues("archwing", archwingsNames, "name", "warframes");
+getValues("necramech", necramechsNames, "name", "warframes");
+getModsData();
+getWeaponsData();
+
+console.log(warframesNames);
+console.log(warframesPrimeNames);
+console.log(archwingsNames);
+console.log(necramechsNames);
