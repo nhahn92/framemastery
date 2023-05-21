7149204ff19c69f97b13fe56920c1d4bbfb8b7ac
Nicole Hahn <nicolehahn92@gmail.com>
2023-05-21 16:51:46 -0400

    Cleaned up function

--- scripts/api-retrieval/get-warframes.js
+++ scripts/api-retrieval/get-warframes.js
@@ -1,45 +1,22 @@
 // This function retrieves all data about Warframes, Archwings, and Necramechs
-// Passing "regular," "prime," "archwing," or "necramech" as this function's parameter will return a specific array
+// Passing one of the following parameters will return a specific collection:
+// "regular," "prime"
 async function getWarframesData(type) {
     try {
         const response = await fetch("https://api.warframestat.us/warframes/");
         const json = await response.json();
 
-        // Four empty arrays to be filled with filtered objects
-        let warframesArray = [];
-        let warframesPrimeArray = [];
-        let archwingsArray = [];
-        let necramechsArray = [];
+        // New collections filled with filtered objects
+        let warframesArray = json.filter(element => element.productCategory === "Suits" && element.isPrime === false);
+        let warframesPrimeArray = json.filter(element => element.productCategory === "Suits" && element.isPrime === true);
 
-        json.forEach(element => {
-            // Filters out Archwings and Necramechs
-            if (element.productCategory === "Suits") {
-                // Further filters out Prime Warframes to make a new array, "warframesArray"
-                if (element.isPrime === false) {
-                    warframesArray.push(element);
-                // Further filters out non-Prime Warframes to make a new array, "warframesPrimeArray"
-                } else if (element.isPrime === true) {
-                    warframesPrimeArray.push(element);
-                }
-            // Filters out Warframes and Necramechs to make a new array, "archwingsArray"
-            } else if (element.productCategory === "SpaceSuits") {
-                archwingsArray.push(element);
-            // Filters out Warframes and Archwings to make a new array, "necramechsArray"
-            } else if (element.productCategory === "MechSuits") {
-                necramechsArray.push(element);
-            }
-        });
-
-        // Returns an array based on passed parameter
+        // Returns a collection based on passed parameter
         if (type === "regular") {
             return warframesArray;
         } else if (type === "prime") {
             return warframesPrimeArray;
-        } else if (type === "archwing") {
-            return archwingsArray;
-        } else if (type === "necramech") {
-            return necramechsArray;
         }
+
     } catch (err) {
         console.log(err);
     }
