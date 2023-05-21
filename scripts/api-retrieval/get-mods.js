fe74a2c6f9fe8465cbf8a1d958e5f6860efc40ed
Nicole Hahn <nicolehahn92@gmail.com>
2023-05-21 16:51:58 -0400

    Completed function with filtering options

--- scripts/api-retrieval/get-mods.js
+++ scripts/api-retrieval/get-mods.js
@@ -1,12 +1,66 @@
 // This function retrieves all data about Mods
-async function getModsData() {
+// Passing one of the following parameters will return a specific collection:
+// "all", "warframe," "primary," "secondary," "melee," "archwing," "archgun,"
+// "archmelee," "necramech," "companion," "kdrive," "plexus," "parazon," "stance,"
+// "set"
+async function getModsData(type) {
     try {
         const response = await fetch("https://api.warframestat.us/mods/");
         const json = await response.json();
-        return json;
+        
+        // New collections filled with filtered objects
+        let warframeModsArray = json.filter(element => element.type === "Warframe Mod");
+        let primaryModsArray = json.filter(element => element.type === "Primary Mod" || element.type === "Shotgun Mod");
+        let secondaryModsArray = json.filter(element => element.type === "Secondary Mod");
+        let meleeModsArray = json.filter(element => element.type === "Melee Mod");
+        let archwingModsArray = json.filter(element => element.type === "Archwing Mod");
+        let archgunModsArray = json.filter(element => element.type === "Arch-Gun Mod");
+        let archmeleeModsArray = json.filter(element => element.type === "Arch-Melee Mod");
+        let necramechModsArray = json.filter(element => element.type === "Necramech Mod");
+        let companionModsArray = json.filter(element => element.type === "Companion Mod");
+        let kdriveModsArray = json.filter(element => element.type === "K-Drive Mod");
+        let plexusModsArray = json.filter(element => element.type === "Plexus Mod");
+        let parazonModsArray = json.filter(element => element.type === "Parazon Mod");
+        let stanceModsArray = json.filter(element => element.type === "Stance Mod");
+        let setModsArray = json.filter(element => element.type === "Mod Set Mod");
+
+        // Returns a collection based on passed parameter
+        if (type === "all") {
+            return json;
+        } else if (type === "warframe") {
+            return warframeModsArray;
+        } else if (type === "primary") {
+            return primaryModsArray;
+        } else if (type === "secondary") {
+            return secondaryModsArray;
+        } else if (type === "melee") {
+            return meleeModsArray;
+        } else if (type === "archwing") {
+            return archwingModsArray;
+        } else if (type === "archgun") {
+            return archgunModsArray;
+        } else if (type === "archmelee") {
+            return archmeleeModsArray;
+        } else if (type === "necramech") {
+            return necramechModsArray;
+        } else if (type === "companion") {
+            return companionModsArray;
+        } else if (type === "kdrive") {
+            return kdriveModsArray;
+        } else if (type === "plexus") {
+            return plexusModsArray;
+        } else if (type === "parazon") {
+            return parazonModsArray;
+        } else if (type === "stance") {
+            return stanceModsArray;
+        } else if (type === "set") {
+            return setModsArray;
+        } 
+
     } catch (err) {
         console.log(err);
     }
 }
 
+getModsData();
 export { getModsData };
--- scripts/api-retrieval/get-weapons.js
+++ scripts/api-retrieval/get-weapons.js
@@ -1,9 +1,44 @@
 // This function retrieves all data about Weapons
-async function getWeaponsData() {
+// Passing one of the following parameters will return a specific collection:
+// "primary," "secondary," "melee," "archgun," "archmelee," "zaw," "kitgun,"
+// "sentinel," "amp"
+async function getWeaponsData(type) {
     try {
-        const response = await fetch("https://api.warframestat.us/weapons/");
+        const response = await fetch("https://api.warframestat.us/items/");
         const json = await response.json();
-        return json;
+
+        // New collections filled with filtered objects
+        let primaryWeaponsArray = json.filter(element => element.category === "Primary" && element.type !== "Companion Weapon");
+        let secondaryWeaponsArray = json.filter(element => element.category === "Secondary");
+        let meleeWeaponsArray = json.filter(element => element.category === "Melee" && element.type !== "Zaw Component");
+        let archgunWeaponsArray = json.filter(element => element.category === "Arch-Gun");
+        let archmeleeWeaponsArray = json.filter(element => element.category === "Arch-Melee" && element.name !== "Corufell");
+        let zawsArray = json.filter(element => element.type === "Zaw Component" && element.uniqueName.includes("Tip") && !element.uniqueName.includes("PvPVariant"));
+        let kitgunsArray = json.filter(element => element.type === "Kitgun Component" && element.uniqueName.includes("Barrel") || element.uniqueName.includes("Barrels"));
+        let sentinelWeaponsArray = json.filter(element => element.type === "Companion Weapon");
+        let ampsArray = json.filter(element => element.type === "Amp" && element.uniqueName.includes("Barrel") || element.name === "Sirocco");
+
+        // Returns a collection based on passed parameter
+        if (type === "primary") {
+            return primaryWeaponsArray;
+        } else if (type === "secondary") {
+            return secondaryWeaponsArray;
+        } else if (type === "melee") {
+            return meleeWeaponsArray;
+        } else if (type === "archgun") {
+            return archgunWeaponsArray;
+        } else if (type === "archmelee") {
+            return archmeleeWeaponsArray;
+        } else if (type === "zaw") {
+            return zawsArray;
+        } else if (type === "kitgun") {
+            return kitgunsArray;
+        } else if (type === "sentinel") {
+            return sentinelWeaponsArray;
+        } else if (type === "amp") {
+            return ampsArray;
+        }
+
     } catch (err) {
         console.log(err);
     }
