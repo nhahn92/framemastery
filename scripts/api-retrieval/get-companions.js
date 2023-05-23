66d8bdfcf7426d809053aa44f71fa51cbd4ea79d
Nicole Hahn <nicolehahn92@gmail.com>
2023-05-22 21:31:21 -0400

    Added "all" parameter

--- scripts/api-retrieval/get-companions.js
+++ scripts/api-retrieval/get-companions.js
@@ -1,17 +1,20 @@
 // This function retrieves all data about Companions
 // Passing one of the following parameters will return a specific collection:
-// "beast," "robotic"
+// "all," "beast," "robotic"
 async function getCompanionsData(type) {
     try {
         const response = await fetch("https://api.warframestat.us/items/");
         const json = await response.json();
 
         // New collections filled with filtered objects
+        let allCompanionsArray = json.filter(element => ["Pets", "Sentinel"].includes(element.type) || element.category === "Pets" && element.name.includes("Venari"));
         let beastsArray = json.filter(element => element.type === "Pets" && !element.uniqueName.includes("Zanuka") || element.category === "Pets" && element.name.includes("Venari"));
         let roboticsArray = json.filter(element => element.type === "Sentinel" || element.type === "Pets" && element.uniqueName.includes("Zanuka"));
 
         // Returns a collection based on passed parameter
-        if (type === "beast") {
+        if (type === "all") {
+            return allCompanionsArray;
+        } else if (type === "beast") {
             return beastsArray;
         } else if (type === "robotic") {
             return roboticsArray;
