2455011136a673dc6fa01fe3090d3de14d283315
Nicole Hahn <nicolehahn92@gmail.com>
2023-05-22 21:32:00 -0400

    Added "all" parameter

--- scripts/api-retrieval/get-warframes.js
+++ scripts/api-retrieval/get-warframes.js
@@ -1,17 +1,20 @@
 // This function retrieves all data about Warframes, Archwings, and Necramechs
 // Passing one of the following parameters will return a specific collection:
-// "regular," "prime"
+// "all," "regular," "prime"
 async function getWarframesData(type) {
     try {
         const response = await fetch("https://api.warframestat.us/warframes/");
         const json = await response.json();
 
         // New collections filled with filtered objects
+        let allWarframesArray = json.filter(element => element.productCategory === "Suits");
         let warframesArray = json.filter(element => element.productCategory === "Suits" && element.isPrime === false);
         let warframesPrimeArray = json.filter(element => element.productCategory === "Suits" && element.isPrime === true);
 
         // Returns a collection based on passed parameter
-        if (type === "regular") {
+        if (type === "all") {
+            return allWarframesArray;
+        } else if (type === "regular") {
             return warframesArray;
         } else if (type === "prime") {
             return warframesPrimeArray;
